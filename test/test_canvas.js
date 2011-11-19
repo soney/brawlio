51b63cda8fb350b5d5b32efd8a70469260cfa25d
Stephen Oney <swloney@gmail.com>
2011-11-19 12:39:54 -0500

    Added more shapes for the test canvas

--- test/test_canvas.js
+++ test/test_canvas.js
@@ -8,6 +8,7 @@
 		this.canvas = document.createElement('canvas');
 		this.width = options.width;
 		this.height = options.height;
+		this.scale = options.scale;
 		this.canvas.setAttribute("width", this.get_width());
 		this.canvas.setAttribute("height", this.get_height());
 		this.ctx = this.canvas.getContext("2d");
@@ -26,11 +27,21 @@
 			this.ctx.arc(cx, cy, r, 0, Math.PI*2, true);
 		};
 		proto.draw = function(shape, options) {
+			options = options || {};
 			this.save();
+			this.ctx.scale(this.scale, this.scale);
+
+			this.ctx.strokeStyle = options.strokeStyle || "black";
+			this.ctx.fillStyle = options.fillStyle || "black";
+			this.ctx.lineWidth = options.lineWidth || 1/this.scale;
+
 			if(shape.is("line")) {
 				var line = shape;
 				if(close_to(line.b, 0)) { //Approximately vertical
 					var x = -line.c/line.a;
+					this.ctx.beginPath();
+					this.ctx.moveTo(x, -1);
+					this.ctx.lineTo(x, this.get_height()+1);
 				} else {
 					var point_at = function(x) {
 						var y = (-line.c - line.a*x)/line.b;
@@ -38,8 +49,70 @@
 					};
 					var p1 = point_at(-1);
 					var p2 = point_at(this.get_width()+1);
-					console.log(p1, p2);
+
+					this.ctx.beginPath();
+					this.ctx.moveTo(p1.x, p1.y);
+					this.ctx.lineTo(p2.x, p2.y);
 				}
+				this.ctx.stroke();
+			} else if(shape.is("line_segment")) {
+				var line_segment = shape;
+
+				this.ctx.beginPath();
+				this.ctx.moveTo(line_segment.p0.x, line_segment.p0.y);
+				this.ctx.lineTo(line_segment.p1.x, line_segment.p1.y);
+				
+				this.ctx.stroke();
+				this.ctx.beginPath();
+				this.circle(line_segment.p0.x, line_segment.p0.y, 2/this.scale);
+				this.ctx.stroke();
+				this.ctx.beginPath();
+				this.circle(line_segment.p1.x, line_segment.p1.y, 2/this.scale);
+				this.ctx.stroke();
+			} else if(shape.is("ray")) {
+				var ray = shape;
+
+				this.ctx.beginPath();
+				this.circle(ray.p0.x, ray.p0.y, 2/this.scale);
+				this.ctx.stroke();
+				var theta = ray.get_theta();
+				var p2x = ray.p0.x + (this.get_width() + this.get_height()) * Math.cos(theta);
+				var p2y = ray.p0.y + (this.get_width() + this.get_height()) * Math.sin(theta);
+
+				this.ctx.beginPath();
+				this.ctx.moveTo(ray.p0.x, ray.p0.y);
+				this.ctx.lineTo(p2x, p2y);
+				
+				this.ctx.stroke();
+			} else if(shape.is("vector")) {
+				var vector = shape;
+				var x0 = options.x || 0;
+				var y0 = options.y || 0;
+				var x1 = x0 + vector.x;
+				var y1 = y0 + vector.y;
+
+				this.ctx.beginPath();
+				this.ctx.moveTo(x0, y0);
+				this.ctx.lineTo(x1, y1);
+				this.ctx.stroke();
+				var theta = vector.get_theta();
+				var arrow_length = 4;
+				var arrow_p1x = x1 + arrow_length * Math.cos(theta + 3*Math.PI/4);
+				var arrow_p1y = y1 + arrow_length * Math.sin(theta + 3*Math.PI/4);
+
+				var arrow_p2x = x1 + arrow_length * Math.cos(theta - 3*Math.PI/4);
+				var arrow_p2y = y1 + arrow_length * Math.sin(theta - 3*Math.PI/4);
+				this.ctx.beginPath();
+				this.ctx.moveTo(x1,y1);
+				this.ctx.lineTo(arrow_p1x, arrow_p1y);
+				this.ctx.lineTo(arrow_p2x, arrow_p2y);
+				this.ctx.closePath();
+				this.ctx.fill();
+			} else if(shape.is("circle")) {
+				var circle = shape;
+
+				this.ctx.beginPath();
+				this.circle(circle.get_cx(), circle.get_cy(), circle.get_radius());
 			}
 			this.restore();
 		};
