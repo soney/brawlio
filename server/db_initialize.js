114994fc6795664e1ab69c225b692e211fb384d1
Stephen Oney <swloney@gmail.com>
2011-12-20 04:41:11 -0800

    The king is dead

--- server/db_initialize.js
+++ server/db_initialize.js
@@ -3,9 +3,5 @@ var database = require('./database').database;
 database.drop_tables(function() {
 	database.create_tables(function() {
 		database.close();
-		database.set_king(1,
-			function() {
-				console.log("done");
-			});
 	});
 });
